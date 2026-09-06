d6e63313c255b69b6fb6839bba6acffd8257f095
aadil <zabboy250@gmail.com>
2026-09-07 00:06:08 +0530

    made the thing yes

---
+++ script.js
@@ -0,0 +1,35 @@
+const countdownElement = document.getElementById('countdown');
+
+const dday = new Date(2026,8,12).getTime();
+
+const units = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
+
+function timeUntilDday() {
+
+  const now = new Date().getTime();
+
+  const diff = dday - now;
+
+  const time = {}
+
+  time.years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+  time.months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30)) % 12;
+  time.days = Math.floor(diff / (1000 * 60 * 60 * 24)) % 30;
+  time.hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+  time.minutes = Math.floor(diff / (1000 * 60)) % 60;
+  time.seconds = Math.floor(diff / 1000) % 60;
+
+  return time;
+}
+
+function updateCountdown() {
+  const time = timeUntilDday();
+  units.forEach(unit => {
+    countdownElement.children[unit].textContent = time[unit];
+  })
+}
+
+
+setInterval(() => {
+  updateCountdown();
+}, 1000);
